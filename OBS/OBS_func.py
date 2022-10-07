ad850c62bf4809e6d02de8e37786f2c3b3c592f0
joaoeduardo <109396949+joapedu@users.noreply.github.com>
2022-10-07 15:02:43 -0300

    Create OBS_func.py

---
+++ OBS/OBS_func.py
@@ -0,0 +1,17 @@
+import math
+
+def say_hi(name, age):
+  print("Hello " + name + ", you are " + age)
+
+say_hi("Mike", "35")
+
+say_hi("Steve", "70")
+## definição básica da função por meio do print (aplicado a duas funções[name][age])
+
+def cube(num):
+  return num*num*num
+
+resultado = cube(3)
+
+print(resultado)
+## return tem função de parar o código, além de definilo como um todo
