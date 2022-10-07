5bbcf83d3b8d31da7a94717defc49429373b069f
joaoeduardo <109396949+joapedu@users.noreply.github.com>
2022-10-07 15:02:56 -0300

    Create OBS_list.py

---
+++ OBS/OBS_list.py
@@ -0,0 +1,32 @@
+import math
+
+lista = []
+lista1 = []
+lista2 = []
+
+lista1.extend(lista2)
+## adciona uma lista a outra
+
+lista.insert(1, "arroz")
+## adciona um elemento no lugar a sua escolha(lista começa em 0)
+
+lista.remove("arroz")
+## remove um elemento da lista
+
+lista.clear()
+## limpa a lista - deixa em branco
+
+lista.index("arroz")
+## fala a posição do elemento "arroz" dentro da lista.
+
+lista.count("arroz")
+## conta quantas vezes aparece o elemento arroz dentro da lista
+
+lista.sort()
+## coloca a lista:
+## em ordem alfabética(LETRAS)
+## em ordem crescente(NÚMEROS)
+
+lista.reverse
+## reverte a ordem da lista
+
